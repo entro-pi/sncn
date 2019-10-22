abc1e67bd4e642a0867ac056fe7594bce2ed8897
Entropy <taylorbelland@gmail.com>
2019-10-21 21:26:40 -0700

    Handles deps
    Sets up the project

--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"context"
+	"time"
+  "go.mongodb.org/mongo-driver/mongo"
+  "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
   fmt.Println("Hallo")
-
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		panic(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		panic(err)
+	}
 }
