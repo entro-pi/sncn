fc24c00e8423f73e9b7697743935eef349aaa827
Taylor Belland <taylorbelland@gmail.com>
2019-11-22 15:12:53 -0500

    implements a (watched) file concatenator

--- idlesnow/idle.ex
+++ idlesnow/idle.ex
@@ -1,5 +1,15 @@
 defmodule Heart do
-  def print(lang) do
-    IO.puts "Hello!"<>lang
-  end
+  	def print(lang) do
+		IO.puts("Hello!"<>lang)
+	end
+	def notify(mess) do
+		{:ok, file} = File.open("../pot/broadcasts", [:read, :write])
+		{:ok, oldcontents } = File.read("../pot/broadcasts")
+		IO.binwrite(file, oldcontents <> mess)
+		File.close(file)
+	end
+	def see do
+		File.read("../pot/broadcasts")
+	end
+
 end
