36d369b0e110c6a01f2170cdde12a7b9bc35e216
Taylor Belland <taylorbelland@gmail.com>
2019-11-22 10:58:51 -0500

    implements a file watcher

--- idlecrash/main.go
+++ idlecrash/main.go
@@ -1,10 +1,44 @@
 package main
 
 import (
-	"fmt"
-//	"github.com/fsnotify/fsnotify"
+	"log"
+	"github.com/fsnotify/fsnotify"
 )
 
 func main() {
-	fmt.Println("Insert a file system monitoring decoder")
+
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+	    log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+
+	go func() {
+	    for {
+	        select {
+	        case event, ok := <-watcher.Events:
+	            if !ok {
+	                return
+	            }
+	            log.Println("event:", event)
+	            if event.Op&fsnotify.Write == fsnotify.Write {
+	                log.Println("modified file:", event.Name)
+	            }
+	        case err, ok := <-watcher.Errors:
+	            if !ok {
+	                return
+	            }
+	            log.Println("error:", err)
+	        }
+	    }
+	}()
+
+	err = watcher.Add("../pot")
+	if err != nil {
+	    log.Fatal(err)
+	}
+	<-done
 }
