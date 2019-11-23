7f127ea2ce6a8c07159d79de4a39030210e95264
Entropy <taylorbelland@gmail.com>
2019-11-22 22:34:02 -0800

    floop

--- idlesnow/idle.ex
+++ idlesnow/idle.ex
@@ -38,7 +38,7 @@ defmodule Connector do
 		{ok, connection} = AMQP.Connection.open("amqp://guest:guest@localhost")
 		{:ok, channel} = AMQP.Channel.open(connection)
 
-		AMQP.Queue.declare(channel, "input")
+		AMQP.Queue.declare(channel, "input", auto_delete: true)
 		AMQP.Basic.consume(channel, "input", nil, no_ack: true)
 
 		IO.puts " [*] Waiting for messages. To exit press CTRL+C, CTRL+C"
