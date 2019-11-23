1e0bdea420c37b91987ff4727dae6bdca2a8ed01
Entropy <taylorbelland@gmail.com>
2019-11-22 21:12:38 -0800

    starts the amqp server and client set up

--- idlesnow/idle.ex
+++ idlesnow/idle.ex
@@ -19,3 +19,26 @@ defmodule Heart do
 	end
 
 end
+Application.ensure_started(:amqp_client)
+
+defmodule Listen do
+	def wait_for_messages do
+		receive do
+			{:basic_deliver, payload, _meta} ->
+			IO.puts " [x] Received #{payload}"
+
+			wait_for_messages()
+		end
+	end
+end
+
+
+{:ok, connection} = AMQP.Connection.open("amqp://guest:guest@dev.snowcrash.network")
+{:ok, channel} = AMQP.Channel.open(connection)
+
+AMQP.Queue.declare(channel, "hello")
+AMQP.Basic.consume(channel, "hello", nil, no_ack: true)
+
+IO.puts " [*] Waiting for messages. To exit press CTRL+C, CTRL+C"
+
+Listen.wait_for_messages()
--- idlesnow/mix.exs
+++ idlesnow/mix.exs
@@ -14,13 +14,18 @@ defmodule Idlesnow.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      application: [
+	:amqp, :amqp_client, :logger
+	]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+	# {:gossip, "~> 1.4"},
+	{:amqp_client, "~> 3.7.11"},
+	{:amqp, "~> 1.3"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
