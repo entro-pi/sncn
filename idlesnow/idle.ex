75f82ec1af175e5cebf3a2eb3c75a8eabcc3e6c1
Entropy <taylorbelland@gmail.com>
2019-11-22 21:49:07 -0800

    instantiates rabbitmq on local

--- idlesnow/idle.ex
+++ idlesnow/idle.ex
@@ -31,14 +31,18 @@ defmodule Listen do
 		end
 	end
 end
+defmodule Connector do
 
+	def connect do
 
-{:ok, connection} = AMQP.Connection.open("amqp://guest:guest@dev.snowcrash.network")
-{:ok, channel} = AMQP.Channel.open(connection)
+		{ok, connection} = AMQP.Connection.open("amqp://guest:guest@localhost")
+		{:ok, channel} = AMQP.Channel.open(connection)
 
-AMQP.Queue.declare(channel, "hello")
-AMQP.Basic.consume(channel, "hello", nil, no_ack: true)
+		AMQP.Queue.declare(channel, "hello")
+		AMQP.Basic.consume(channel, "hello", nil, no_ack: true)
 
-IO.puts " [*] Waiting for messages. To exit press CTRL+C, CTRL+C"
+		IO.puts " [*] Waiting for messages. To exit press CTRL+C, CTRL+C"
 
-Listen.wait_for_messages()
+		Listen.wait_for_messages()
+	end
+end
