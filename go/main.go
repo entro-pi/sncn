0c65ff8459d190484441663f933cd5ccf3a91373
Entropy <taylorbelland@gmail.com>
2019-11-23 17:33:33 -0800

    flopping in the golang monitor

--- go/main.go
+++ go/main.go
@@ -85,6 +85,59 @@ func doInput(input string) {
 }
 
 
+func actOn() {
+        connection := getConnectionString()
+        conn, err := amqp.Dial(connection)
+
+        failOnError(err, "Failed to connect to RabbitMQ")
+
+        defer conn.Close()
+
+        ch, err := conn.Channel()
+
+        failOnError(err, "Failed to open a channel")
+
+        defer ch.Close()
+
+        q, err := ch.QueueDeclare(
+                "input", //name
+                true, // durable
+                true, //delete when used
+                false, //exclusive
+                false, //no-wait
+                nil, //arguments
+        )
+        failOnError(err, "Failed to declare a queue")
+	forever := make(chan bool)
+	for {
+
+	        msgs, err := ch.Consume(
+			q.Name, //queue
+			"",
+			true, //auto-ack
+			false, //exclusive
+			false, //no-local
+			false, //no-wait
+			nil, //args
+		)
+	        failOnError(err, "Failed to register a consumer")
+		go func() {
+			for d := range msgs {
+				log.Printf("Received a message: %s", d.Body)
+				f, err := os.Open("../pot/broadcast")
+				if err != nil {
+					panic(err)
+				}
+				defer f.Close()
+				f.WriteString("\n")
+			}
+
+		}()
+		<-forever
+
+
+	}
+}
 
 func watch() {
 	var broadcastContainer []string
